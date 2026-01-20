12159b9ee108a40022e4ed0a1d5606e6b5d27da3
PierpaoloSpadafora <pierpaolospadafora23+github@gmail.com>
2026-01-20 22:26:13 +0100

    quick push

---
+++ web-ext-config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+    ignoreFiles: [
+        "temp_page.html",
+        "node_modules",
+        "web-ext-artifacts",
+        "docs",
+        "package.json",
+        "package-lock.json",
+        "web-ext-config.js"
+    ]
+};
